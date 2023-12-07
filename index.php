dff999814fc9065c93a6cf4216bf19a749fc008c
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-12-07 17:03:52 -0500

    new redst0ne.xyz site
    based on squarebracket frontend

---
+++ index.php
@@ -0,0 +1,238 @@
+
+<!doctype html>
+<html lang="en" class="h-100" data-bs-theme="auto">
+  <head><script src="/docs/5.3/assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>redst0ne.xyz</title>
+    
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
+<link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link href="style.css" rel="stylesheet">
+<link href="ardosia.css" rel="stylesheet">
+
+<meta name="theme-color" content="#712cf9">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        width: 100%;
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+
+      .btn-bd-primary {
+        --bd-violet-bg: #712cf9;
+        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+        --bs-btn-font-weight: 600;
+        --bs-btn-color: var(--bs-white);
+        --bs-btn-bg: var(--bd-violet-bg);
+        --bs-btn-border-color: var(--bd-violet-bg);
+        --bs-btn-hover-color: var(--bs-white);
+        --bs-btn-hover-bg: #6528e0;
+        --bs-btn-hover-border-color: #6528e0;
+        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+        --bs-btn-active-color: var(--bs-btn-hover-color);
+        --bs-btn-active-bg: #5a23c8;
+        --bs-btn-active-border-color: #5a23c8;
+      }
+
+      .bd-mode-toggle {
+        z-index: 1500;
+      }
+
+      .bd-mode-toggle .dropdown-menu .active .bi {
+        display: block !important;
+      }
+
+      #frontJumbo {
+    margin-top: -24px !important;
+}
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+
+    
+<header>
+  <!-- Fixed navbar -->
+  <nav id="navbar" class="navbar  navbar-light bg-light  navbar-static-top navbar-expand-md">
+		<div class="container">
+			<a class="navbar-brand" href="."><img src="xyzlogo.png" alt="redst0ne.xyz" height="32" class="d-inline-block align-text-top"></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div id="navbarCollapse" class="collapse navbar-collapse">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+				<ul class="navbar-nav dropdown-center">
+<script src="common.js"></script> 
+										<!-- <div class="dropdown">
+						<li class="nav-item">
+							<style>
+								#openSettings.nav-link {
+									padding: 0.25rem;
+								}
+							</style>
+							<a href="#" id="openSettings" class="dropdown-toggle nav-link" data-bs-toggle="dropdown" aria-expanded="false">
+								<svg class="bi" width="30" height="30" fill="currentColor">
+	<use xlink:href="icons.svg#three-dots-vertical"/>
+</svg>							</a>
+						</li>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdown" id="mainMenu">
+														<li><a class="dropdown-item" id="themeSelect" href="#"><svg class="bi" width="16" height="16" fill="currentColor">
+	<use xlink:href="icons.svg#brush"/>
+</svg> Change theme</a></li>
+													</ul>
+						<ul class="dropdown-menu" id="themeSelection" aria-labelledby="navbarDropdown">
+							 <li><a class="dropdown-item" id="back" href="#">Back</a></li>
+							<li><a class="dropdown-item" id="finalium" href="#">Light</a></li>
+							<li><a class="dropdown-item" id="light" href="#">Classic</a></li>
+						</ul>
+					</div>  -->
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</nav>
+    <div class="mb-4 subnav">
+	<nav class="py-1 bg-dark">
+		<div class="container">
+			<ul class="nav me-auto">
+				<li class="nav-item"><a href="." class="nav-link link-light px-2"><svg class="bi" width="16" height="16" fill="currentColor">
+	<use xlink:href="icons.svg#house-door"/>
+</svg> Home</a></li>
+<li class="nav-item"><a href="." class="nav-link link-light px-2"><img style="filter: invert(1);" class="bi" width="16" height="16" src="sb24.svg">
+</svg> squareBracket</a></li>
+							</ul>
+		</div>
+	</nav>
+      </div>
+</header>
+<div id="frontJumbo" class="p-5 mb-4 bg-purple text-white" style="margin-top:-16px;">
+      <div class="container">
+	  				<h1 class="display-5 fw-bold">Welcome to redst0ne.xyz</h1>
+				<p class="col-xl-8 fs-4">I made squareBracket (not to be confused with Qobo).</p>
+				<p>	</p><div class="btn-group">
+				<a href="//squarebracket.pw" class="btn btn-primary btn-lg">squareBracket</a>
+						<a href="//https://discord.gg/CvF4bSAdp9" class="btn btn-light btn-lg">Discord</a>
+					</div>
+				<p></p>		      </div>
+    </div>
+<!-- Begin page content -->
+  <div class="container">
+    <h1>Projects</h1>
+    <p class="lead">A short list of projects I've worked on</p>
+    <br>
+    <div>
+    <div class="card" style="margin-top:-30px">
+	<div class="card-body">
+		<div class="row">
+			<div class="col-4">
+				<a href="//squarebracket.pw">
+					<div class="img-thumbnail">
+						<img class="img-fluid" width="960" height="540" src="sbsmall.png">
+					</div>
+				</a>
+			</div>
+			<div class="col-6">
+				<h3><a href="//squarebracket.pw">squareBracket</a></h3>
+																					<p>			<a class="user text-muted" href="//squarebracket.pw">https://squarebracket.pw</a>
+				</p>
+				<p>A video sharing site, based off the site formerly bearing the same name (now known as Qobo).</p>
+			</div>
+		</div>
+	</div>
+</div>
+                            </div>
+
+                            <div class="card" style="margin-top:10px">
+	<div class="card-body">
+		<div class="row">
+			<div class="col-4">
+				<a href="//revid.pw">
+					<div class="img-thumbnail">
+						<img class="img-fluid" width="960" height="540" src="revid.png">
+					</div>
+				</a>
+			</div>
+			<div class="col-7">
+				<h3><a href="//revid.pw">Revid</a></h3>
+																					<p>			<a class="user text-muted" href="//revid.pw">https://revid.pw</a>
+				</p>
+				<p>Video sharing site based on 2007 YouTube, using a highly improved version of the RETROTube source code.</p>
+			</div>
+		</div>
+	</div>
+</div>
+                            </div>
+  </div>
+
+<footer>
+<div class="p-3">
+			<div class="container">
+			<div class="card footer">
+			<div class="card-body">
+			<h4 class="my-0">redst0ne.xyz</h4>
+            <p class="my-0"><b>redst0ne.xyz</b> · 2021-2023<!-- | <b>Users registered</b>: 0 | <b>Videos uploaded</b>: 0--></p>
+			<p class="my-0"><a href="about.php" class="me-1">About</a> <a href="credits.php" class="me-1">Credits</a></p>
+			</div>
+			</div>
+		</footer>
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    </body>
+</html>
